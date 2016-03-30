309898638c7bc93d0cc94c3b589cfc3d2f741237
Kirsten Lambertsen <kirsten@kirstenlambertsen.com>
2016-03-29 23:57:39 -0400

    adds xpnkID field to json object

--- xpnk_instagram/xpnk_createInstaJSON/createInstaJSON.go
+++ xpnk_instagram/xpnk_createInstaJSON/createInstaJSON.go
@@ -6,7 +6,8 @@ Takes a group ID and writes all Instagram posts for that group to a json file us
 (1) get all the user id's for a single group
 (2) get all the instagram user id's for each of those user ids
 (3) get all the instagrams associated with each of those instagram_id's
-(4) json-encode all the instagrams and write to a file using a naming convention
+(4) put all the user's instagrams into an object keyed by user's Xpnk_ID
+(5) json-encode all the instagrams and write to a file using a naming convention
 **************************************************************************************/
 
 import (
@@ -26,10 +27,10 @@ type Groupname struct {
     GroupName		string	`db:"group_name"`
 }
 
-//stores the Instagram user names and avatars for each GroupMemberID
+//stores the Instagram user name and XpnkID for each GroupMemberID
 type GroupInstagrammer struct {
+	XpnkID			string
     InstagramUserID	string	`db:"insta_userid"`
-    ProfileImage 	string	`db:"instagram_avatar" json:"profile_image"`
 }
 
 //stores the Xapnik user_ID's of each member of a group
@@ -48,16 +49,22 @@ type XpnkInstagram struct {
 	ProfileImageURL	string	`db:"instagram_avatar" 	json:"profile_image"`
 }
 
+type UserInstagrams struct {
+	XpnkID			string
+	InstagramPosts	[]XpnkInstagram
+}
+
 func CreateInstaJSON(group_id int) string {
 	
 	dbmap := initDb()
 	defer dbmap.Db.Close()
 	
 	var group_name Groupname
-
-	//converting the group name into a string to use in the filename
-	//get group name to use for the filename	
 	
+	/******
+	* converting the group name into a string to use in the filename
+	******/
+	//get group name to use for the filename	
 	err := dbmap.SelectOne(&group_name, "SELECT `group_name` FROM `GROUPS` WHERE `Group_ID`=?", group_id)
 
 	if err != nil {fmt.Printf("There was an error ", err)}
@@ -75,31 +82,44 @@ func CreateInstaJSON(group_id int) string {
 
 	fmt.Printf("\n==========\nGROUP NAME IS NOW:%+v\n",this_name)
 
-	//get all the xpnk user_ID's associated with the Group_ID from USER_GROUPS
-
+	/******
+	* get all the xpnk user_ID's associated with the Group_ID from USER_GROUPS
+	******/
 	var group_members []GroupMemberID
 
 	_, err = dbmap.Select(&group_members, "SELECT `user_ID` FROM `USER_GROUPS` WHERE `Group_ID`=?", group_id)
 
-	//fmt.Printf("\n==========\nMember ID's:%+v\n",group_members)
+	fmt.Printf("\n==========\nMember ID's:%+v\n",group_members)
 
 	checkErr(err, "Select failed")
-
-	//get the insta_userid from USERS for each user_ID in group_members
-
+	/****
+	* get the insta_userid from USERS for each user_ID in group_members
+	****/
 	var group_instagrammers []GroupInstagrammer
 
 	for i := 0; i < len(group_members); i++ {
 
 		instagrammer := group_members[i].UserID
-
-		_, err := dbmap.Select(&group_instagrammers, "SELECT `insta_userid` FROM `USERS` WHERE `user_ID`=?", instagrammer)
+				
+		thisInstaId, err := dbmap.SelectStr("SELECT `insta_userid` FROM `USERS` WHERE `user_ID`=?", instagrammer)
+		
+		fmt.Printf("\n==========\nTHIS INSTAGRAMMER:%+v\n==========\n",thisInstaId)
+		
+		var thisInstagrammer GroupInstagrammer
+		
+		thisInstagrammer.InstagramUserID = thisInstaId
+		thisInstagrammer.XpnkID = instagrammer
+		
+		group_instagrammers = append(group_instagrammers, thisInstagrammer)
 
 		checkErr(err, "Select failed")
 	}
-
-	//write the Instagram user names to a file using file-naming convention
-
+	
+	fmt.Printf("\n==========\nINSTAGRAMMERS:%+v\n==========\n",group_instagrammers)
+	
+	/******
+	* write the Instagram user names to a file using file-naming convention
+	******/
 	this_users, err := os.Create("/Users/mizkirsten/Desktop/Node/XAPNIK/data/"+this_name+"_insta_users.json")
 	
 	//convert group_instagrammers struct to json
@@ -109,17 +129,34 @@ func CreateInstaJSON(group_id int) string {
 	}
 
 		this_users.WriteString(string(users_str))
+	/*******
+	* get all the group Instagram posts from the db
+	*******/ 
+	var group_instagrams []UserInstagrams
+	
+	var this_user  UserInstagrams
 
-	//get all the group Instagram posts from the db
-
-	var group_instagrams []XpnkInstagram
-
+	var user_instagrams []XpnkInstagram
+	
 	for i := 0; i < len(group_instagrammers); i++ {
-
+			
 		instagrammer := group_instagrammers[i].InstagramUserID
+		
+		this_user.XpnkID = group_instagrammers[i].XpnkID
 
-		_, err := dbmap.Select(&group_instagrams, "SELECT * FROM `instagram_posts` WHERE `insta_userid`=?", instagrammer)
-
+		fmt.Printf("\n==========\nINSTAGRAMMER:%+v\n==========\n",this_user.XpnkID)
+		
+		_, err := dbmap.Select(&user_instagrams, "SELECT * FROM `instagram_posts` WHERE `insta_userid`=?", instagrammer)
+		
+		this_user.InstagramPosts = user_instagrams
+		
+		fmt.Printf("\n==========\nTHIS USERS POSTS:%+v\n==========\n",this_user)
+		
+		group_instagrams = append(group_instagrams, this_user)
+		
+		user_instagrams = []XpnkInstagram{} 
+		//this has to be emptied or it carries over to the next instagrammer
+				
 		checkErr(err, "Select failed")
 
 	}
