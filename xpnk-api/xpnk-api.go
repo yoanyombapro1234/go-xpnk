63aa6671994a0f96dd51a6bc7fc101d237f5f529
Kirsten Lambertsen <kirsten@kirstenlambertsen.com>
2019-04-06 13:11:04 -0400

    api refactor add new member to group

--- xpnk-api/xpnk-api.go
+++ xpnk-api/xpnk-api.go
@@ -613,29 +613,7 @@ func GroupID (c *gin.Context) {
 		c.JSON(422, gin.H{"error": "No groupname was sent."})
 	}		 
 }
-/*
-func GroupsAddMember (c *gin.Context) {
-	var new_groupMember		NewGroupMember
-	c.Bind(&new_groupMember)
-	fmt.Printf("\n new_groupMember.Group_ID:  %v \n", new_groupMember.Group_ID)
-	fmt.Printf("new_groupMember.User_ID:  %v \n", new_groupMember.User_ID)
-	if new_groupMember.Group_ID < 1 || new_groupMember.User_ID < 1 {
-		c.JSON(422, gin.H{"error": "Group id or user id or both are missing."})
-	} else {
-		var new_member_insert   NewGroupMemberInsert
-		new_member_insert.Group_ID	= new_groupMember.Group_ID
-		new_member_insert.User_ID	= new_groupMember.User_ID
-	
-		insert_new_member 			:= InsertNewGroupMember(new_member_insert)
-	
-		if insert_new_member == 1 {
-			c.JSON(201, "User added!")
-		} else {
-			c.JSON(422, gin.H{"error": insert_new_member })
-		}
-	}
-}
-*/
+
 func XPNKReadHeader (c *gin.Context) int{
 	var this_header http.Header
 	this_header = c.Request.Header
@@ -936,24 +914,7 @@ func updateDisqus(new_Disqusauth NewDisqusAuth) string{
 	
 	return "updated"
 } 
-/*
-func InsertNewGroupMember(new_GroupMember NewGroupMemberInsert) int {
-	var returnVal 				int
-	dbmap := db_connect.InitDb()
-	defer dbmap.Db.Close()
-	dbmap.AddTableWithName(NewGroupMemberInsert{}, "USER_GROUPS")
-		
-	err := dbmap.Insert(&new_GroupMember)	
-		if err == nil {
-			fmt.Printf("\n==========\nInsertNewGroupMember Update Success!")
-			returnVal = 1
-		} else {
-			fmt.Printf("\n==========\nProblemz with InsertNewGroupMember line 700 in api: \n%+v\n",err)
-			returnVal = 0
-		}
-	return returnVal	
-}
-*/
+
 /*
 func getGroup (groupID string) []int{
 	var groupUsers			[]int
